b3295c314a904f561d5238b6a0d9c394e603fa24
semenkulemin <55997982+semenkulemin@users.noreply.github.com>
2023-04-06 02:08:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
-from scipy.stats import chi2
+# from scipy.stats import norm
+# from scipy.stats import chi2
+from scipy.stats import expon
 
 chat_id = 341395919 # Ваш chat ID, не меняйте название переменной
 
@@ -12,7 +13,9 @@ def solution(p: float, x: np.array) -> tuple:
     # Не меняйте название функции и её аргументы
     x = x / 25
     alpha = 1 - p
-    return 2 * np.sum(x) / chi2.ppf(1 - alpha / 2, 2 * len(x)), 2 * np.sum(x) / chi2.ppf(alpha / 2, 2 * len(x))
+    left = min(1/2 - expon.ppf(alpha) / (n * (min(x) / 5**2)), 1/2 - expon.ppf(1 - alpha) / (n * (min_x / 5**2)))
+    right = max(1/2 - expon.ppf(alpha) / (n * (min(x) / 5**2)), 1/2 - expon.ppf(1 - alpha) / (n * (min_x / 5**2)))
+    return left, right
     
 #     alpha = 1 - p
 #     loc = x.mean()
