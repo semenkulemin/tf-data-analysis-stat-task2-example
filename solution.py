6b8a02843424481b7cc357468c793532926a6286
semenkulemin <55997982+semenkulemin@users.noreply.github.com>
2023-04-06 12:39:17 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # from scipy.stats import norm
 # from scipy.stats import chi2
-from scipy.stats import expon
+# from scipy.stats import expon
 
 chat_id = 341395919 # Ваш chat ID, не меняйте название переменной
 
@@ -12,9 +12,12 @@ def solution(p: float, x: np.array) -> tuple:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    l = min(1/2 - expon.ppf(alpha) / (n * (min(x) / 5**2)), 1/2 - expon.ppf(1 - alpha) / (n * (min_x / 5**2)))
-    r = max(1/2 - expon.ppf(alpha) / (n * (min(x) / 5**2)), 1/2 - expon.ppf(1 - alpha) / (n * (min_x / 5**2)))
-    return l, r
+    return (-min(-x) - 1 / 2) / (5**2 / 2), \
+            (-np.log(alpha) / len(x) -min(-x) - 1 / 2) / (5**2 / 2)
+    
+#     l = min(1/2 - expon.ppf(alpha) / (n * (min(x) / 5**2)), 1/2 - expon.ppf(1 - alpha) / (n * (min_x / 5**2)))
+#     r = max(1/2 - expon.ppf(alpha) / (n * (min(x) / 5**2)), 1/2 - expon.ppf(1 - alpha) / (n * (min_x / 5**2)))
+#     return l, r
     
 #     alpha = 1 - p
 #     loc = x.mean()
